da2c4393c084ad808f3c3922fbec0772b576482c
Jonathan Adelson <3988280+samhoice@users.noreply.github.com>
2022-10-26 22:57:21 -0400

    Updated installer and vimrc

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ VIMDIR=$HOME/.vim
 
 echo $VIMDIR
 
+mkdir -p $VIMDIR
+mkdir -p $VIMDIR/swap
+
+# vim
 echo "linking vimrc"
 ln -s -f $SOURCEDIR/vim/vimrc $VIMDIR/vimrc
+# TODO: there's a bug here:
+# echo "linking vim/autoload/"
+# ln -s -f $SOURCEDIR/vim/autoload $VIMDIR/autoload
+# echo "linking vim/ftplugin/"
+# ln -s -f $SOURCEDIR/vim/ftplugin $VIMDIR/ftplugin
 
-echo "linking vim/autoload/"
-ln -s -f $SOURCEDIR/vim/autoload $VIMDIR/autoload
-echo "linking vim/ftplugin/"
-ln -s -f $SOURCEDIR/vim/ftplugin $VIMDIR/ftplugin
+# tmux
+ln -s -f $SOURCEDIR/tmux.conf $HOME/.tmux.conf 
