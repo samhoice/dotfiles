b73eb098ac6d89742f1e6543afae6c073496dd2d
Jon Adelson <3988280+samhoice@users.noreply.github.com>
2026-02-01 15:27:19 -0500

    Adds a bunch of emacs packages

--- emacs/init.el
+++ emacs/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven))
  '(org-agenda-files '("~/notes/tasks.org"))
- '(package-selected-packages '(helm magit)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,3 +24,51 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
+
+(use-package vertico
+  :ensure
+  :init (vertico-mode))
+
+(use-package orderless
+  :ensure
+  :custom
+  (completion-styles '(orderless basic)))
+
+(use-package consult
+  :ensure
+  :bind (("C-c f" . consult-fd)))
+
+(use-package markdown-mode
+  :mode "\\.md\\'")
+
+;; Javascript and Typescript
+(use-package typescript-mode
+  :ensure
+  :mode "\\.ts\\'"
+  :config
+  (setq typescript-indent-level 2))
+
+;; (use-package js2-mode
+;;   :ensure
+;;   :mode "\\.js\\'"
+;;   :config
+;;   (setq js2-basic-offset 2))
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((typescript-mode . lsp-deferred)
+         (tsx-ts-mode . lsp-deferred)
+         (js-mode . lsp-deferred))
+  :commands lsp-deferred
+  :bind ((:map lsp-mode-map
+               ("C-c r" . lsp-rename)
+               ("C-c a" . lsp-execute-code-action)
+               ("C-c d" . lsp-find-definition)
+               ("C-c i" . lsp-find-implementation)))
+  :config
+  (setq lsp-headerline-breadcrumb-enable nil))
+
+
+
+(use-package apheleia
+  :ensure t)
