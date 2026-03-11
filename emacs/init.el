ed879669fe2144d27735d616aec4807fe5b9f7ea
Jonathan Adelson <3988280+samhoice@users.noreply.github.com>
2026-03-11 08:14:57 -0400

    Win: emacs.el Adds M-o to be other-window

--- emacs/init.el
+++ emacs/init.el
@@ -26,6 +26,7 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "M-o") #'other-window)
 
 ;; package manager
 (require 'package)
